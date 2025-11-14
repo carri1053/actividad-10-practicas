db747a1555c3ec7182573f36e29bfd0553b5d754
carri1053 <rodrigocarrillo2506@gmail.com>
2025-11-13 21:39:14 -0300

    Add files via upload

--- base/pages/04_✅ Usos digitales.py
+++ base/pages/04_✅ Usos digitales.py
@@ -16,7 +16,7 @@ conteo_edad.plot(kind='pie', ax=ax)
 ax.set_label('Edad')
 ax.set_title('Edades de los jóvenes que usan las redes sociales')
 
-with st.expander("En este grafico vemos las edades de los jovenes que usan las redes sociales"):
+with st.expander("En este gráfico vemos las edades de los jóvenes que usan las redes sociales."):
      st.pyplot(fig)
 
 fig,ax = plt.subplots()
@@ -24,9 +24,9 @@ genv = dig[dig["Género"]== "Varón"]
 vi = genv["youtube"].value_counts()
 vi.plot(kind='pie', ax=ax)
 
-ax.set_title('Uso de la app de youtube en los chicos')
+ax.set_title('Uso de la app de YouTube en los chicos.')
 
-with st.expander("Grafico de Youtube en los chicos de la encuesta"):
+with st.expander("Gráfico del uso de la app YouTube en los chicos encuestados."):
      st.pyplot(fig)
 
 fig,ax = plt.subplots()
@@ -34,9 +34,9 @@ genv = dig[dig["Género"]== "Varón"]
 vi = genv["twiter"].value_counts()
 vi.plot(kind='pie', ax=ax)
 
-ax.set_title('Uso de la app de twiter en los chicos')
+ax.set_title('Uso de la app de Twitter en los chicos.')
 
-with st.expander("Grafico de twiter en los chicos de la encuesta"):
+with st.expander("Gráfico del uso de la app Twitter en los chicos encuestados."):
      st.pyplot(fig)
      
 fig,ax = plt.subplots()
@@ -44,8 +44,18 @@ genv = dig[dig["Género"]== "Varón"]
 vi = genv["tiktok"].value_counts()
 vi.plot(kind='pie', ax=ax)
 
-ax.set_title('Uso de la app de tiktok en los chicos')
+ax.set_title('Uso de la app de TikTok en los chicos.')
 
-with st.expander("Grafico de tiktok en los chicos de la encuesta"):
+with st.expander("Gráfico del uso de la app TikTok en los chicos encuestados."):
+     st.pyplot(fig)
+
+fig,ax = plt.subplots()
+genv = dig[dig["Género"]== "Varón"]
+vi = genv["instagram"].value_counts()
+vi.plot(kind='pie', ax=ax)
+
+ax.set_title('Uso de la app de Instagram en los chicos.')
+
+with st.expander("Gráfico del uso de la app Instagram en los chicos encuestados."):
      st.pyplot(fig)
      
